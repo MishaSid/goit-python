b20370bfbd0ea5fad628edb47b1280f672721367
Mykhailo Sydorchuk <mishasydorchuk@gmail.com>
2021-04-04 14:20:11 +0300

    code ammended according to mentors comment

--- lesson3/hw3.py
+++ lesson3/hw3.py
@@ -1,14 +1,14 @@
 def main(n):
-    if __name__ == '__main__':
-        return n
+    return n
 
 def fibonacci(n):
-
     if n == 0:
         return 0
     elif n <= 2:
         return 1       
     else:
         return fibonacci(n-1) + fibonacci(n-2)      
-    
-print(fibonacci(3))
+
+if __name__ == '__main__':    
+    n = int(input('Enter number: '))
+    print(fibonacci(n))
