e403991a82e6a28bff660b62e39dc55b10579a65
Mykhailo Sydorchuk <mishasydorchuk@gmail.com>
2021-04-18 14:35:52 +0300

    HW5 final

--- lesson5/HW5.py
+++ lesson5/HW5.py
@@ -5,10 +5,13 @@
 
 
 def normalize(text):
-    dictionary = {ord('А'): 'A',ord('Б'): 'B',ord('В'): 'V',ord('Г'): 'H',ord('Ґ'): 'G',ord('Д'): 'D',ord('Е'): 'E',ord('Є'): 'Ye',ord('Ж'): 'Zh',ord('З'): 'Z',ord('И'): 'Y',ord('І'): 'I',ord('Ї'): 'Yi',ord('Й'): 'Y',\
-    ord('К'): 'K',ord('Л'): 'L',ord('М'): 'M',ord('Н'): 'N',ord('О'): 'O',ord('П'): 'P',ord('Р'): 'R',ord('С'): 'S',ord('Т'): 'T',ord('У'): 'U',ord('Ф'): 'F',ord('Х'): 'Kh',ord('Ц'): 'Ts',ord('Ч'): 'Ch',ord('Ш'): 'Sh',ord('Щ'): 'Shch',\
-    ord('Ь'): '',ord('Ю'): 'Yu',ord('Я'): 'Ya',ord('а'): 'a',ord('б'): 'b',ord('в'): 'v',ord('г'): 'h',('ґ'): 'g',ord('д'): 'd',ord('е'): 'e',ord('є'): 'ie',ord('ж'): 'zh',ord('з'): 'z',ord('и'): 'y',ord('і'): 'i',ord('ї'): 'i',ord('й'): 'i',\
-    ord('к'): 'k',ord('л'): 'l',ord('м'): 'l',ord('н'): 'n',ord('о'): 'o',ord('п'): 'p',ord('р'): 'r',ord('с'): 's',ord('т'): 't',ord('у'): 'u',ord('ф'): 'f',ord('х'): 'kh',ord('ц'): 'ts',ord('ч'): 'ch',ord('ш'): 'sh',ord('щ'): 'shch',ord('ь'): '',ord('ю'): 'iu',ord('я'): 'ia'}
+    dictionary = {ord('А'): 'A',ord('Б'): 'B',ord('В'): 'V',ord('Г'): 'H',ord('Ґ'): 'G',ord('Д'): 'D',ord('Е'): 'E',ord('Є'): 'Ye',ord('Ж'): 'Zh',\
+    ord('З'): 'Z',ord('И'): 'Y',ord('І'): 'I',ord('Ї'): 'Yi',ord('Й'): 'Y',ord('К'): 'K',ord('Л'): 'L',ord('М'): 'M',ord('Н'): 'N',ord('О'): 'O',\
+    ord('П'): 'P',ord('Р'): 'R',ord('С'): 'S',ord('Т'): 'T',ord('У'): 'U',ord('Ф'): 'F',ord('Х'): 'Kh',ord('Ц'): 'Ts',ord('Ч'): 'Ch',ord('Ш'): 'Sh',\
+    ord('Щ'): 'Shch',ord('Ь'): '',ord('Ю'): 'Yu',ord('Я'): 'Ya',ord('а'): 'a',ord('б'): 'b',ord('в'): 'v',ord('г'): 'h',('ґ'): 'g',ord('д'): 'd',\
+    ord('е'): 'e',ord('є'): 'ie',ord('ж'): 'zh',ord('з'): 'z',ord('и'): 'y',ord('і'): 'i',ord('ї'): 'i',ord('й'): 'i',ord('к'): 'k',ord('л'): 'l',\
+    ord('м'): 'l',ord('н'): 'n',ord('о'): 'o',ord('п'): 'p',ord('р'): 'r',ord('с'): 's',ord('т'): 't',ord('у'): 'u',ord('ф'): 'f',ord('х'): 'kh',\
+    ord('ц'): 'ts',ord('ч'): 'ch',ord('ш'): 'sh',ord('щ'): 'shch',ord('ь'): '',ord('ю'): 'iu',ord('я'): 'ia'}
     keys = []
     for key in dictionary:
         keys.append(key)
